9ff33febe2788a741dbd0e06fdc0a360298c9286
Natalino Gomes <intgomes657@gmail.com>
2025-11-02 13:15:13 +0000

    store, and provider

--- Slices/productsApiSlice.js
+++ Slices/productsApiSlice.js
@@ -1,2 +1,26 @@
 import { PRODUCT_URL } from "../constants/Urls";
 import { apiSlice } from "./apiSlice";
+
+export const productsApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getProducts: builder.query({
+      query: ({ keyword, pageNumber }) => ({
+        url: PRODUCT_URL,
+        params: { keyword, pageNumber },
+      }),
+      keepUnusedDataFor: 5,
+      providesTags: ["Product"],
+    }),
+
+    getProductsDetails: builder.query({
+      query: (productId) => ({
+        url: `${PRODUCT_URL}/${productId}`,
+      }),
+      keepUnusedDataFor: 5,
+      providesTags: ["Product"],
+    }),
+  }),
+});
+
+export const { useGetProductsQuery, useGetProductsDetailsQuery } =
+  productsApiSlice;
--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,10 +1,14 @@
 import { Stack } from "expo-router";
+import { Provider } from "react-redux";
+import store from "../store";
 
 const RootLayout = () => {
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
+    <Provider store={store}>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+    </Provider>
   );
 };
 
